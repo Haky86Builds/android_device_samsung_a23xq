e438981eeef093bafd584240cb8ca2b24c079b5c
Jaekyun Seok <jaekyun@google.com>
2025-08-11 11:31:03 +0200

    a23xq: Enforce RROs for all build-time overlays
    Change-Id: I8f0cad5dae869545eda2b2ec70971e54cef0b777

--- device.mk
+++ device.mk
@@ -86,6 +86,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# Overlay
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Partitions
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
