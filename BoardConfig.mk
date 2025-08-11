448f2256aa71f72d720786b6a0eb738737dae1de
jmpfbmx <jmpf.bmx@gmail.com>
2025-08-11 10:56:59 +0200

    a23xq: Add arch, bootloader and platform flags
    Change-Id: Ie0ca1d06a57a715867ceb4954cc37248cd099081
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,12 +5,32 @@
 
 DEVICE_PATH := device/samsung/a23xq
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := kryo385
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a75
+
 # Assert
 TARGET_OTA_ASSERT_DEVICE := a23xq
 
 # Board
 TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 TARGET_BOARD_NAME := SRPVC22C00A
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := holi
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_BOARD_PLATFORM := holi
 
 # Security Patch Level
 VENDOR_SECURITY_PATCH := 2025-04-01
