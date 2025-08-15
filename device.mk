126aeb1d6cfd5c1df5d47e22e4d19c0a66e0978f
Jack Yu <jackcwyu@google.com>
2025-08-16 00:37:09 +0200

    a23xq: Create feature flags for OMAPI readers
    Bug: 131062577
    Test: CTS OMAPI
    Change-Id: I69f3f913fdf3daf49ac74fede4e61282d1ff9d55

--- device.mk
+++ device.mk
@@ -21,6 +21,11 @@ PRODUCT_PACKAGES += \
 BOARD_SHIPPING_API_LEVEL := 31
 PRODUCT_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.ese.xml \
+    frameworks/native/data/etc/android.hardware.se.omapi.sd.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.sd.xml \
+    frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl.samsung-sm6375 \
