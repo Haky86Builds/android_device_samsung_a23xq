691df7d15340b574046ad571ff6744e7b3816a73
Francescodario Cuzzocrea <bosconovic@gmail.com>
2025-08-16 00:23:50 +0200

    a23xq: Build fastcharge hidl from source
    Change-Id: Iec1a7faec24f0d5b4b1d2f1ef52b781368c5bd51

--- device.mk
+++ device.mk
@@ -163,6 +163,10 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.1-impl-mock \
     fastbootd
 
+# Fastcharge
+PRODUCT_PACKAGES += \
+    vendor.lineage.fastcharge@1.0-service.samsung 
+
 # File System
 PRODUCT_PACKAGES += \
     fs_config_files
