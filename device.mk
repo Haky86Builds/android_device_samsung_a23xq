0a348672132920a7ae3ec7c2f91a9047c6152b26
Eric Laurent <elaurent@google.com>
2025-08-15 18:24:48 +0200

    a23xq: Add Bluetooth Hearing Aid routing and configuration
    Bug: 69623109
    Test: connect to Hearing Aid, make phone call, verify audio is heard
    Change-Id: I92267fb48608fb721c5c4ef7ec9fceeafc8ea192

--- device.mk
+++ device.mk
@@ -21,6 +21,7 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(DEVICE_PATH)/audio/configs/,$(TARGET_COPY_OUT_VENDOR)/etc/) \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/hearing_aid_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/hearing_aid_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
 
