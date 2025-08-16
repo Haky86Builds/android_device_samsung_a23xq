c69d299214373167126e5e8f67f01cdcdf150b0e
Haky86 <hakamelassouad@gmail.com>
2025-08-16 11:57:14 +0200

    a23xq: Drop TARGET_EXCLUDES_AUDIOFX
    Change-Id: Id40e258729b7cbf0bc2c2fd85abee6001308f252

--- device.mk
+++ device.mk
@@ -69,8 +69,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
-TARGET_EXCLUDES_AUDIOFX := true
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0 \
